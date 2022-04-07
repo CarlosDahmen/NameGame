b29162fb3e47b2277df5902dd3832e59f30d02e6
CarlosDahmen <carlos.dahmen@gmail.com>
2022-04-07 15:46:33 -0400

    Rewrite game & people redux stores w/ TypeScript

--- src/store/game.tsx
+++ src/store/game.tsx
@@ -1,6 +1,16 @@
-import axios from 'axios'
 
-const initialState = {
+interface IInitialState {
+  round: number;
+  score: number;
+}
+
+interface actionType {
+  type: string;
+  score?: number;
+  round?: number;
+}
+
+const initialState: IInitialState = {
   round: 1,
   score: 0,
 }
@@ -12,17 +22,17 @@ const RESET_GAME = 'RESET_GAME';
 
 
 //Action Creators
-export const updateScore = score => ({
+export const updateScore = (score: number): actionType => ({
   type: UPDATE_SCORE,
   score
 })
 
-export const updateRounds = round => ({
+export const updateRounds = (round: number): actionType => ({
   type: UPDATE_ROUNDS,
   round: round + 1
 })
 
-export const resetGame = () => ({
+export const resetGame = (): actionType => ({
   type: RESET_GAME,
   round: 1,
   score: 0
@@ -31,8 +41,7 @@ export const resetGame = () => ({
 
 
 //Reducer
-const gameReducer = (state = initialState, action) => {
-  console.log(action)
+const gameReducer = (state = initialState, action: actionType) => {
   switch(action.type) {
     case UPDATE_SCORE:
       return {...state, score: action.score };
--- src/store/people.tsx
+++ src/store/people.tsx
@@ -1,4 +1,36 @@
 import axios from 'axios'
+import { Dispatch } from 'redux';
+
+interface personObj {
+  firstName: string;
+  headshot: {
+    alt: string;
+    height: number;
+    id:string;
+    mimeType: string;
+    type: string;
+    url: string;
+    width: number
+  };
+  id: string;
+  jobTitle: string;
+  lastName: string;
+  slug: string;
+  socialLinks: string[];
+  type: string
+}
+
+interface IInitialState {
+  people: personObj[];
+
+}
+
+interface actionType {
+  type: string;
+  people: personObj[];
+  roundPeople: personObj[];
+  realPerson: personObj;
+}
 
 const initialState = {
   people: [],
@@ -7,24 +39,24 @@ const initialState = {
 };
 
 //Action Constant
-const GET_PEOPLE = 'GET_PEOPLE';
-const GENERATE_ROUND_PEOPLE = 'GENERATE_ROUND_PEOPLE';
-const SELECT_REAL_PERSON = 'SELECT_REAL_PERSON';
+const GET_PEOPLE:string = 'GET_PEOPLE';
+const GENERATE_ROUND_PEOPLE:string = 'GENERATE_ROUND_PEOPLE';
+const SELECT_REAL_PERSON:string = 'SELECT_REAL_PERSON';
 
 //Action Creator
-export const getPeople = people => ({
+export const getPeople = (people: personObj[]) => ({
   type: GET_PEOPLE,
   people
 })
 
-export const generateRoundPeople = (roundPeople) => {
+export const generateRoundPeople = (roundPeople: personObj[]) => {
     return {
       type: GENERATE_ROUND_PEOPLE,
       roundPeople
     }
 }
 
-export const selectRealPerson = (roundPeople) => {
+export const selectRealPerson = (roundPeople: personObj[]) => {
   return {
     type: SELECT_REAL_PERSON,
     realPerson: roundPeople[Math.floor(Math.random() * roundPeople.length)]
@@ -32,10 +64,10 @@ export const selectRealPerson = (roundPeople) => {
 }
 
 //Thunk (gets people from api)
-export const fetchPeople = () => async dispatch => {
+export const fetchPeople = () => async( dispatch: Dispatch) => {
     const { data } = await axios.get('https://namegame.willowtreeapps.com/api/v1.0/profiles');
     // Clean data by removing people objects that have no image url or have a default image
-    let cleanArray = data.filter(peopleObj =>
+    let cleanArray = data.filter((peopleObj: personObj) =>
       peopleObj.headshot.url &&
       peopleObj.headshot.alt !== "Logo" &&
       peopleObj.headshot.url !== "https://namegame.willowtreeapps.com/images/featured-image-TEST1.png"
@@ -45,7 +77,7 @@ export const fetchPeople = () => async dispatch => {
   dispatch(getPeople(cleanArray));
 
   // Select 6 people randomly from the people array
-  let roundPeople = [];
+  let roundPeople: personObj[] = [];
     while(roundPeople.length < 6){
       let person = cleanArray[Math.floor(Math.random() * cleanArray.length)]
       if (!roundPeople.includes(person)) {
@@ -59,7 +91,7 @@ export const fetchPeople = () => async dispatch => {
 }
 
 //Reducer
-const peopleReducer = (state = initialState, action) => {
+const peopleReducer = (state = initialState, action: actionType) => {
   switch(action.type) {
     case GET_PEOPLE:
       return {...state, people: action.people};
